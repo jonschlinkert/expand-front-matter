9e0aac41a9400ae43ae4606f33a167aeff7781e1
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-14 05:24:01 -0400

    0.2.0 tests

--- test.js
+++ test.js
@@ -2,27 +2,62 @@
 var assert = require('assert');
 var should = require('should');
 var expand = require('./');
-var Assemble = require('assemble').Assemble;
-var assemble;
+var matter = require('parser-front-matter');
+var assemble = require('assemble');
+var loader = require('assemble-loader');
+var app;
 
 
 describe('expand', function () {
   beforeEach(function () {
-    assemble = new Assemble();
-    assemble.defaultConfig();
+    app = assemble();
+    app.use(loader());
+    app.create('pages');
+
+    app.onLoad(/./, function (view, next) {
+      matter.parse(view, next);
+    });
   });
 
   it('should expand config templates in front matter:', function () {
-    assemble.data({title: 'test'});
-    assemble.onLoad(/./, expand(assemble));
-    assemble.pages('fixtures/*.md');
-    assert(assemble.views.pages['fixtures/a.md'].data.title === 'test');
+    app.data({title: 'test'});
+    app.onLoad(/./, expand(app));
+    app.pages('fixtures/*.md');
+    assert(app.views.pages['fixtures/a.md'].data.title === 'test');
   });
 
   it('should support locals as the second argument:', function () {
-    assemble.data({title: 'test'});
-    assemble.onLoad(/./, expand(assemble, {title: 'something else'}));
-    assemble.pages('fixtures/*.md');
-    assert(assemble.views.pages['fixtures/a.md'].data.title === 'something else');
+    app.data({title: 'test'});
+    app.onLoad(/./, expand(app, {title: 'something else'}));
+    app.pages('fixtures/*.md');
+    assert(app.views.pages['fixtures/a.md'].data.title === 'something else');
+  });
+
+  it('should work as a "view" plugin:', function () {
+    app.data({title: 'test'});
+    app.pages('fixtures/*.md');
+
+    app.pages.getView('fixtures/a.md')
+      .use(expand(app))
+
+    assert(app.views.pages['fixtures/a.md'].data.title === 'test');
+  });
+
+  it('should work as an "app" plugin:', function () {
+    app.data({title: 'test'});
+    app.use(expand());
+    app.create('posts');
+
+    app.post('fixtures/a.md');
+    assert(app.views.posts['fixtures/a.md'].data.title === 'test');
+  });
+
+  it('should work as a "collection" plugin:', function () {
+    app.data({title: 'test'});
+    app.create('posts')
+      .use(expand(app));
+
+    app.post('fixtures/a.md');
+    assert(app.views.posts['fixtures/a.md'].data.title === 'test');
   });
 });
