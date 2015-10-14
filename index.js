98f456a6c1fbb3526c0657033a08adf2047e8897
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-14 05:23:49 -0400

    update to work with latest assemble
    These updates also allow this to be used as a plugin or middleware.

--- index.js
+++ index.js
@@ -7,16 +7,31 @@
 
 'use strict';
 
-var merge = require('mixin-deep');
-var expand = require('expand');
+var utils = require('./utils');
 
 module.exports = function (app, locals) {
-  return function (view, next) {
+  var opts = (app && app.options) || {};
+  var cache = (app && app.cache) || {};
+  var expand = utils.expand(opts);
+  var data = utils.merge({}, cache.data, locals);
+
+  return function fn(view, next) {
+    if (!view.isView) {
+      utils.merge(data, view.cache.data);
+      return fn;
+    }
+
+    if (typeof next !== 'function') {
+      next = function () {
+        return view;
+      };
+    }
+
     if (!view.data || view.data.process === false) {
       return next();
     }
-    var data = merge({}, app.cache.data, locals);
+
     view.data = expand(view.data, data);
-    next();
+    return next();
   };
 };
