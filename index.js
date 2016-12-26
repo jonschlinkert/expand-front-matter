813fd0aeff05cc032fb3858e32bc284ee2f77329
jonschlinkert <jon.schlinkert@sellside.com>
2016-12-26 15:51:00 -0500

    refactor

--- index.js
+++ index.js
@@ -7,31 +7,71 @@
 
 'use strict';
 
-var utils = require('./utils');
+var isObject = require('isobject');
+var isValid = require('is-valid-app');
+var extend = require('extend-shallow');
+var merge = require('mixin-deep');
+var expand = require('expand');
 
-module.exports = function (app, locals) {
-  var opts = (app && app.options) || {};
-  var cache = (app && app.cache) || {};
-  var expand = utils.expand(opts);
-  var data = utils.merge({}, cache.data, locals);
+module.exports = function(app, locals) {
+  if (!isObject(app) || (!app.isApp && !app.isCollection)) {
+    locals = app;
+    app = {cache: {}, options: {}};
+  }
 
-  return function fn(view, next) {
-    if (!view.isView) {
-      utils.merge(data, view.cache.data);
+  var data = extend({}, app.cache.data, locals);
+  var merged = {};
+  var opts = {};
+
+  return function fn(file, next) {
+    if (!isValid(file, 'expand-front-matter', ['view', 'file'])) {
+      if (this.options.expandData === false) {
+        return;
+      }
+
+      if (this.isCollection && !merged[this._name]) {
+        merged[this._name] = {
+          data: extend({}, this.cache.data),
+          opts: extend({}, this.options)
+        };
+      } else if (this.isApp && !merged.app) {
+        merged.app = {
+          opts: extend({}, this.options, opts),
+          data: merge({}, this.cache.data, data)
+        };
+      }
       return fn;
     }
 
-    if (typeof next !== 'function') {
-      next = function () {
-        return view;
-      };
-    }
+    var type = file.options.collection;
+    var appData = merged.app ? merged.app.data : {};
+    var typeData = merged[type] ? merged[type].data : {};
+    var mergedData = merge({}, appData, typeData, data);
 
-    if (!view.data || view.data.process === false) {
-      return next();
+    if (file.options.expandData !== false) {
+      file.data = expand(opts)(file.data, mergedData);
     }
+  };
 
-    view.data = expand(view.data, data);
-    return next();
+  if (typeof next === 'function') {
+    next();
+  }
+};
+
+module.exports.middleware = function(app, locals) {
+  if (!isObject(app) || (!app.isApp && !app.isCollection)) {
+    locals = app;
+    app = {cache: {}, options: {}};
+  }
+
+  var data = merge({}, app.cache.data, locals);
+
+  return function(file, next) {
+    var opts = extend({}, app.options, file.options);
+
+    if (opts.expandData !== false) {
+      file.data = expand(opts)(file.data, data);
+    }
+    next();
   };
 };
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-/* deps: mocha */
+'uses strict';
+
+require('mocha');
 var assert = require('assert');
-var should = require('should');
-var expand = require('./');
-var matter = require('parser-front-matter');
 var assemble = require('assemble');
 var loader = require('assemble-loader');
+var matter = require('parser-front-matter');
+var expand = require('./');
 var app;
-
 
 describe('expand', function () {
   beforeEach(function () {
@@ -14,23 +14,46 @@ describe('expand', function () {
     app.use(loader());
     app.create('pages');
 
-    app.onLoad(/./, function (view, next) {
-      matter.parse(view, next);
-    });
+    app.onLoad(/./, matter.parse);
   });
 
-  it('should expand config templates in front matter:', function () {
+  it('should expand config templates in front matter', function () {
     app.data({title: 'test'});
     app.onLoad(/./, expand(app));
     app.pages('fixtures/*.md');
-    assert(app.views.pages['fixtures/a.md'].data.title === 'test');
+    assert.equal(app.pages.getView('a.md').data.title, 'test');
+  });
+
+  it('should expand config templates in front matter with `.middleware`:', function () {
+    app.data({title: 'test'});
+    app.onLoad(/./, expand.middleware(app));
+    app.pages('fixtures/*.md');
+    assert.equal(app.pages.getView('a.md').data.title, 'test');
   });
 
   it('should support locals as the second argument:', function () {
     app.data({title: 'test'});
-    app.onLoad(/./, expand(app, {title: 'something else'}));
+    app.onLoad(/./, expand.middleware(app, {title: 'something else'}));
     app.pages('fixtures/*.md');
-    assert(app.views.pages['fixtures/a.md'].data.title === 'something else');
+    assert.equal(app.pages.getView('a.md').data.title, 'something else');
+  });
+
+  it('should work as an "app" plugin:', function () {
+    app.create('posts');
+    app.data({title: 'test'});
+    app.post('fixtures/a.md');
+    app.use(expand());
+
+    assert.equal(app.posts.getView('a.md').data.title, 'test');
+  });
+
+  it('should work as a "collection" plugin:', function () {
+    app.data({title: 'test'});
+    app.create('posts')
+    app.post('fixtures/a.md');
+    app.posts.use(expand(app));
+
+    assert.equal(app.posts.getView('a.md').data.title, 'test');
   });
 
   it('should work as a "view" plugin:', function () {
@@ -40,24 +63,6 @@ describe('expand', function () {
     app.pages.getView('fixtures/a.md')
       .use(expand(app))
 
-    assert(app.views.pages['fixtures/a.md'].data.title === 'test');
-  });
-
-  it('should work as an "app" plugin:', function () {
-    app.data({title: 'test'});
-    app.use(expand());
-    app.create('posts');
-
-    app.post('fixtures/a.md');
-    assert(app.views.posts['fixtures/a.md'].data.title === 'test');
-  });
-
-  it('should work as a "collection" plugin:', function () {
-    app.data({title: 'test'});
-    app.create('posts')
-      .use(expand(app));
-
-    app.post('fixtures/a.md');
-    assert(app.views.posts['fixtures/a.md'].data.title === 'test');
+    assert.equal(app.pages.getView('a.md').data.title, 'test');
   });
 });
